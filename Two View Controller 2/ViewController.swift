3047d2e4c8a4f0d1ea34c350314e87dcf077dc4a
kanesorn <joeey_wk@hotmail.com>
2016-06-23 10:45:21 +0700

    ViewController Success

--- Two View Controller 2/ViewController.swift
+++ Two View Controller 2/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
+    @IBAction func gotoSecond(sender: AnyObject) {
+        
+        let goToSecond = self.storyboard?.instantiateViewControllerWithIdentifier("kanesornView") as! KanesornViewController
+        self.navigationController?.pushViewController(goToSecond, animated: true)
+        
+        
+    }
 
 }
 
